118aea078cc3e6eaa93a9638d1652a633610bc7c
Alex Ghera <axanderghemov@gmail.com>
2020-02-27 22:56:19 +0100

    test wioth somethiong else

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, flexDirection: 'row' }}>
+      <Text>bla</Text>
+      <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+      <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
+      <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
     </View>
   );
 }
@@ -14,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
